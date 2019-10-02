ce67b82330660f300e2c193cc2a70820a065e9e4
Roig Rosa <roigrosa88@gmail.com>
2019-10-02 04:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*50)
+  cart.push(new Object({itemName:item, itemPrice:price}))
+  console.log(`${item} has been added to your cart.`)
+    return `${item} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
+//write your code here
+   if(getCart().length === 0) {
+  return "Your shopping cart is empty."
+ } else if (getCart().length ===1) {
+  return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
 }
+ const itemsAndPrices = []
+
+   for (let i = 0; i < 1; i++) {
+     let itemAndPrice = cart[i]
+     let item = Object.keys(itemAndPrice)[50]
+     let price = itemAndPrice[item]
+     itemsAndPrices.push(`${item} at \$${price}`)
+
+   }
+   console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
+}
+
 
 function total() {
   // write your code here
